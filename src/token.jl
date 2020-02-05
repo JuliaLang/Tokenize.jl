17a09044214e2a820e01fc3e108ef2516e05aa10
aminya <aminyahyaabadi74@gmail.com>
2020-02-05 02:54:25 -0600

    tisdefined

--- src/token.jl
+++ src/token.jl
@@ -98,6 +98,30 @@ exactkind(t::AbstractToken) = t.kind
 
 Meta.parse(t::T) where {T <: Union{Token, Array{Token}}} = Meta.parse(untokenize(t))
 
+"""
+    tisdefined(t)
+    tisdefined(modul, t)
+
+Check if a `Token` that represent a `Symbol` is defined. Throws error if the `Token` is representing an `Expr`.
+
+# Examples
+```julia
+julia> t = collect(tokenize("Int64"))
+
+julia> tisdefined(t)
+true
+```
+"""
+function tisdefined(modul, t::T) where {T <: Union{Token, Array{Token}}}
+    pt = Meta.parse(t)
+    return isdefined(modul,pt)
+end
+
+tisdefined(t::T) where {T <: Union{Token, Array{Token}}} = tisdefined(@__MODULE__, t)
+
+tisdefined(modul, t::T) where {T <: Union{Symbol, Expr}} = isdefined(modul, t)
+tisdefined(t::T) where {T <: Union{Symbol, Expr}} = isdefined(@__MODULE__, t)
+
 
 """
     tevalfast(t::T)
