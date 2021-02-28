a4e0bca85bf5a662cfbf45384d917e83be77ffba
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2021-02-28 16:59:18 -0500

    Deploy docs with GitHub Actions

--- docs/make.jl
+++ docs/make.jl
@@ -10,9 +10,8 @@ makedocs(
     ],
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/JuliaLang/Tokenize.jl.git",
+    devbranch = "master",
+    push_preview = true,
+)
