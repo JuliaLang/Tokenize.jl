54e035a6dc59343f8ea1cac73abd5fccc2d38c41
KristofferC <kcarlsson89@gmail.com>
2022-02-04 15:52:37 +0100

    remove unused function

--- src/utilities.jl
+++ src/utilities.jl
@@ -54,60 +54,6 @@ const utf8_offset = [0x00000000
 # const EOF_CHAR = convert(Char,typemax(UInt32))
 const EOF_CHAR = typemax(Char)
 
-
-function is_cat_id_start(ch::Char, cat::Integer)
-    c = UInt32(ch)
-    return (cat == Unicode.UTF8PROC_CATEGORY_LU || cat == Unicode.UTF8PROC_CATEGORY_LL ||
-            cat == Unicode.UTF8PROC_CATEGORY_LT || cat == Unicode.UTF8PROC_CATEGORY_LM ||
-            cat == Unicode.UTF8PROC_CATEGORY_LO || cat == Unicode.UTF8PROC_CATEGORY_NL ||
-            cat == Unicode.UTF8PROC_CATEGORY_SC ||  # allow currency symbols
-            cat == Unicode.UTF8PROC_CATEGORY_SO ||  # other symbols
-
-            # math symbol (category Sm) whitelist
-            (c >= 0x2140 && c <= 0x2a1c &&
-             ((c >= 0x2140 && c <= 0x2144) || # ⅀, ⅁, ⅂, ⅃, ⅄
-              c == 0x223f || c == 0x22be || c == 0x22bf || # ∿, ⊾, ⊿
-              c == 0x22a4 || c == 0x22a5 || # ⊤ ⊥
-
-              (c >= 0x2202 && c <= 0x2233 &&
-               (c == 0x2202 || c == 0x2205 || c == 0x2206 || # ∂, ∅, ∆
-                c == 0x2207 || c == 0x220e || c == 0x220f || # ∇, ∎, ∏
-                c == 0x2210 || c == 0x2211 || # ∐, ∑
-                c == 0x221e || c == 0x221f || # ∞, ∟
-                c >= 0x222b)) || # ∫, ∬, ∭, ∮, ∯, ∰, ∱, ∲, ∳
-
-              (c >= 0x22c0 && c <= 0x22c3) ||  # N-ary big ops: ⋀, ⋁, ⋂, ⋃
-              (c >= 0x25F8 && c <= 0x25ff) ||  # ◸, ◹, ◺, ◻, ◼, ◽, ◾, ◿
-
-              (c >= 0x266f &&
-               (c == 0x266f || c == 0x27d8 || c == 0x27d9 || # ♯, ⟘, ⟙
-                (c >= 0x27c0 && c <= 0x27c1) ||  # ⟀, ⟁
-                (c >= 0x29b0 && c <= 0x29b4) ||  # ⦰, ⦱, ⦲, ⦳, ⦴
-                (c >= 0x2a00 && c <= 0x2a06) ||  # ⨀, ⨁, ⨂, ⨃, ⨄, ⨅, ⨆
-                (c >= 0x2a09 && c <= 0x2a16) ||  # ⨉, ⨊, ⨋, ⨌, ⨍, ⨎, ⨏, ⨐, ⨑, ⨒,
-                                                 # ⨓, ⨔, ⨕, ⨖
-                c == 0x2a1b || c == 0x2a1c)))) || # ⨛, ⨜
-
-            (c >= 0x1d6c1 && # variants of \nabla and \partial
-             (c == 0x1d6c1 || c == 0x1d6db ||
-              c == 0x1d6fb || c == 0x1d715 ||
-              c == 0x1d735 || c == 0x1d74f ||
-              c == 0x1d76f || c == 0x1d789 ||
-              c == 0x1d7a9 || c == 0x1d7c3)) ||
-
-            # super- and subscript +-=()
-            (c >= 0x207a && c <= 0x207e) ||
-            (c >= 0x208a && c <= 0x208e) ||
-
-            # angle symbols
-            (c >= 0x2220 && c <= 0x2222) || # ∠, ∡, ∢
-            (c >= 0x299b && c <= 0x29af) || # ⦛, ⦜, ⦝, ⦞, ⦟, ⦠, ⦡, ⦢, ⦣, ⦤, ⦥,
-                                            # ⦦, ⦧, ⦨, ⦩, ⦪, ⦫, ⦬, ⦭, ⦮, ⦯
-            # Other_ID_Start
-            c == 0x2118 || c == 0x212E || # ℘, ℮
-            (c >= 0x309B && c <= 0x309C)) # katakana-hiragana sound marks
-end
-
 function is_identifier_char(c::Char)
     c == EOF_CHAR && return false
     return Base.is_id_char(c)
@@ -117,7 +63,6 @@ function is_identifier_start_char(c::Char)
     c == EOF_CHAR && return false
     return Base.is_id_start_char(c)
 end
-
 
 function peekchar(io::Base.GenericIOBuffer)
     if !io.readable || io.ptr > io.size
