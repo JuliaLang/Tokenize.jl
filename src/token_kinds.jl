72e1baf435d04bf3ffd3afd021b9a43623ae1b48
Kristoffer Carlsson <kcarlsson89@gmail.com>
2022-02-07 11:06:56 +0100

    add two missing Kind -> Symbol (#195)

--- src/token_kinds.jl
+++ src/token_kinds.jl
@@ -1475,3 +1475,12 @@ UNICODE_OPS_REVERSE[DDDOT] = :(...)
 UNICODE_OPS_REVERSE[TRANSPOSE] = Symbol(".'")
 UNICODE_OPS_REVERSE[ANON_FUNC] = :(->)
 UNICODE_OPS_REVERSE[WHERE] = :where
+if VERSION >= v"1.6"
+    str = """
+    begin
+    UNICODE_OPS_REVERSE[LEFT_ARROW] = :(<--)
+    UNICODE_OPS_REVERSE[DOUBLE_ARROW] = :(<-->)
+    end
+    """
+    eval(Meta.parse(str))
+end
