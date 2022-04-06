ac76aaf0e74c0fecfbbe4b01d329aa4e79273d0e
Sebastian Pfitzner <pfitzseb@gmail.com>
2022-04-06 18:18:20 +0200

    Treat UTF-8 BOM as WS

--- src/lexer.jl
+++ src/lexer.jl
@@ -22,7 +22,7 @@ export tokenize
 @inline ishex(c::Char) = isdigit(c) || ('a' <= c <= 'f') || ('A' <= c <= 'F')
 @inline isbinary(c::Char) = c == '0' || c == '1'
 @inline isoctal(c::Char) =  '0' ≤ c ≤ '7'
-@inline iswhitespace(c::Char) = Base.isspace(c)
+@inline iswhitespace(c::Char) = Base.isspace(c) || c === '\ufeff'
 
 mutable struct Lexer{IO_t <: IO, T <: AbstractToken}
     io::IO_t
